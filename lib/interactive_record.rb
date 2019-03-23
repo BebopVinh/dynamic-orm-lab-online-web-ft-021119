8c07b4ac94de901fd42d317fff647fc0d3605059
Vinh Do <vinhdo07@gmail.com>
2019-03-23 03:48:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -33,7 +33,7 @@ class InteractiveRecord
 
   def values_for_insert
     values = self.class.column_names.map do |col_name|
-      "'#{send(col_name)}'" unless send(col_name).nil?
+      "#{send(col_name)}" unless send(col_name).nil?
     end
     binding.pry
   end
